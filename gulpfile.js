7e2c069d9aaad2fdd1b810cfbd929553e51c0a9c
MadDogMarauder <lramirezuoa@hotmail.com>
2016-10-03 23:59:24 -0600

    Added bootstrap to project.
    Note: using amelia theme, this should be removed and replaced with the theme to use for the project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 // Gulp dependencies:
 
 gulp.task('default', function() {
     // Gulp Tasks:
 
+    // Run ESLint
+    gulp.src(["app_server/**/*.js","public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
     //transform es 6 to 5
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
